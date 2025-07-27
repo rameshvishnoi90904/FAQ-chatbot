eebfefebc7e89b33f4373ce5315a2f6120ec905f
rameshvishnoi90904 <rameshvishnoi90904@gmail.com>
2025-07-27 23:14:40 +0800

    fix: update added hints

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from faq import faq_chain, ingest_fag_data
 import streamlit as st
-
+# Load data first
+ingest_fag_data()
 st.sidebar.button("Load FAQ Data", on_click=ingest_fag_data)
 # 1. Title
 st.title("FAQ Chatbot")
 
 # 2. Input box
-input_paragraph = st.text_area("Enter a your query here")
+input_paragraph = st.text_area("Enter a your query here: (Ask about NEFT, RTGS, Amortization, or any banking queries)")
 
 # 3. Button
 extract_button = st.button("Find Answer")
